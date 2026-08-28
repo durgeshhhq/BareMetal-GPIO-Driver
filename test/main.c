0fc37e30eff0bc8260fd94a7b2c45218dae7a7d7
Durgesh Bijalwan <138030007+durgeshhhq@users.noreply.github.com>
2026-08-28 13:13:47 +0530

    Update main.c

--- test/main.c
+++ test/main.c
@@ -1,1 +1,53 @@
+//For a simple GPIO output test, suppose you connect an LED to PA5 with an appropriate resistor.
+#include "stm32f446xx_gpio_driver.h"
 
+static void delay(void)
+{
+    for(volatile uint32_t i = 0; i < 500000U; i++)
+    {
+        __asm volatile ("nop");
+    }
+}
+
+
+int main(void)
+{
+    GPIO_Handle_t GPIO_LED;
+
+    /*
+     * Configure PA5 as GPIO output
+     */
+    GPIO_LED.pGPIOx = GPIOA;
+
+    GPIO_LED.GPIO_PinConfig.GPIO_PinNumber =
+        GPIO_PIN_NO_5;
+
+    GPIO_LED.GPIO_PinConfig.GPIO_PinMode =
+        GPIO_MODE_OUT;
+
+    GPIO_LED.GPIO_PinConfig.GPIO_PinSpeed =
+        GPIO_SPEED_FAST;
+
+    GPIO_LED.GPIO_PinConfig.GPIO_PinPuPdControl =
+        GPIO_NO_PUPD;
+
+    GPIO_LED.GPIO_PinConfig.GPIO_PinOPType =
+        GPIO_OP_TYPE_PP;
+
+    GPIO_LED.GPIO_PinConfig.GPIO_PinAltFunMode =
+        0;
+
+
+    /*
+     * Initialize GPIO
+     */
+    GPIO_Init(&GPIO_LED);
+
+
+    while(1)
+    {
+        GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
+
+        delay();
+    }
+}
